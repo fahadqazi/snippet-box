a67933aa5da7880127f7a73b763d5e66e2b1df14
Fahad Qazi <fahadqazi@gmail.com>
2023-02-23 07:00:32 +0000

    Handle multiplate template files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,14 +14,19 @@ func home(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	t, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = t.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
