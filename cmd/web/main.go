0ea6d758113379403c8b678904b2796f82d46ecb
Fahad Qazi <fahadqazi@gmail.com>
2023-02-24 05:58:24 +0000

    Add cli args & use fileserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
