0198abb9eefda59e2f35dfd08c90603e6fd217ca
Fahad Qazi <fahadqazi@gmail.com>
2023-02-26 06:41:08 +0000

    render tempalte

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"gitlab.com/fqazi/snippet-box/internal/models"
+	"html/template"
 	"net/http"
 	"strconv"
 )
@@ -58,7 +59,22 @@ func (app *application) snippetView(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/view.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, req *http.Request) {
