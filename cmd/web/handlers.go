e915ac5dc8180ec80dadc57e107d98b1c5d13285
Fahad Qazi <fahadqazi@gmail.com>
2023-03-02 06:06:06 +0000

    Update handlers and template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,17 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"gitlab.com/fqazi/snippet-box/internal/models"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -27,7 +23,9 @@ func (app *application) home(w http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, req *http.Request) {
-	id, err := strconv.Atoi(req.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(req.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -50,11 +48,15 @@ func (app *application) snippetView(w http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display form for creating a new snippet ..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, req *http.Request) {
+	//if req.Method != http.MethodPost {
+	//	w.Header().Set("Allow", http.MethodPost)
+	//	app.clientError(w, http.StatusMethodNotAllowed)
+	//	return
+	//}
 
 	title := "o snail"
 	content := "O snail\bClimb mount Fuji,\nBut slowly, slowly,!\n\n- Kobayahi Issa"
@@ -65,5 +67,5 @@ func (app *application) snippetCreate(w http.ResponseWriter, req *http.Request) 
 		app.serverError(w, err)
 	}
 
-	http.Redirect(w, req, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, req, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
