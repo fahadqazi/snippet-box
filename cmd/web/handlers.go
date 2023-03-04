fd2ca163fbf955ea17dab3c50c32c7a69cbae63d
Fahad Qazi <fahadqazi@gmail.com>
2023-03-04 07:25:59 +0000

    use validation helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"gitlab.com/fqazi/snippet-box/internal/models"
+	"gitlab.com/fqazi/snippet-box/internal/validator"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, req *http.Request) {
@@ -77,27 +76,17 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, req *http.Reque
 	}
 
 	form := snippetCreateForm{
-		Title:       req.PostForm.Get("title"),
-		Content:     req.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   req.PostForm.Get("title"),
+		Content: req.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 180), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7, 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must be equal to 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(req)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
