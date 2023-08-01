264be99a78ce715d61dd2608d81af51110d0cdd4
Fahad Qazi <fahadqazi@gmail.com>
2023-08-01 06:04:01 +0100

    Add structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// logs
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
+	// static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Serving on port :4000")
-	err := http.ListenAndServe(":4000", mux)
+	infoLog.Printf("Serving on port :%s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 }
